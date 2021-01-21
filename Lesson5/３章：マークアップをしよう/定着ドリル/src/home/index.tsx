85c1e86669a93b6a5d5dd987f4cf0527d0f13848
kurahashi <koki.kurahashi@di-v.co.jp>
2021-01-21 15:51:07 +0900

    Lesson5解答追加

---
+++ Lesson5/３章：マークアップをしよう/定着ドリル/src/home/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Registration } from "../registration";
+
+export const Home = () => {
+  return (
+    <div className="positon-relative">
+      <Registration/>
+    </div>
+  );
+};
